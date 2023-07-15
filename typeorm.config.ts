ae63ca8e7d3010cf5279b0e795953605212c10b9
Amir Elemam <amirelemam@gmail.com>
2023-07-15 19:50:41 -0300

    db: adds migrations

--- typeorm.config.ts
+++ typeorm.config.ts
@@ -2,6 +2,7 @@ import { DataSource } from 'typeorm';
 import { Menu } from './src/menus/menu.entity';
 import { Restaurant } from './src/restaurants/restaurant.entity';
 import { Dish } from './src/dishes/dish.entity';
+import { Migrations1689396065156 } from './migrations/1689396065156-migrations';
 
 export default new DataSource({
   type: 'postgres',
@@ -12,4 +13,5 @@ export default new DataSource({
   database: 'postgres',
   synchronize: true,
   entities: [Menu, Restaurant, Dish],
+  migrations: [Migrations1689396065156],
 });
