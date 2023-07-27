8d0b8ffdf2852b971ee4bf93974ce070d8137ff1
Amir Elemam <amirelemam@gmail.com>
2023-07-27 07:55:01 -0300

    feature: treats for restaurantId not found

--- src/menus/menu.module.ts
+++ src/menus/menu.module.ts
@@ -3,10 +3,14 @@ import { MenuController } from './menu.controller';
 import { MenuService } from './menu.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Menu } from './menu.entity';
+import { Restaurant } from '../restaurants/restaurant.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Menu])],
+  imports: [
+    TypeOrmModule.forFeature([Menu]),
+    TypeOrmModule.forFeature([Restaurant]),
+  ],
   controllers: [MenuController],
   providers: [MenuService],
 })
-export class MenuModule {}
+export class MenuModule { }
--- src/menus/menu.service.ts
+++ src/menus/menu.service.ts
@@ -1,16 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Menu } from './menu.entity';
 import { MenuDto } from './dto/menu.dto';
+import { Restaurant } from '../restaurants/restaurant.entity';
 @Injectable()
 export class MenuService {
   constructor(
     @InjectRepository(Menu)
     private readonly repository: Repository<Menu>,
+    @InjectRepository(Restaurant)
+    private readonly restaurantRepository: Repository<Restaurant>,
   ) {}
 
-  create(menuData: MenuDto) {
+  async create(menuData: MenuDto) {
+    const restaurant = await this.restaurantRepository.findOne({
+      where: { id: menuData.restaurantId },
+    });
+    if (!restaurant) {
+      throw new NotFoundException(
+        `Restaurant with ID ${menuData.restaurantId} not found`,
+      );
+    }
+
     const menu = new Menu();
 
     Object.assign(menu, menuData);
